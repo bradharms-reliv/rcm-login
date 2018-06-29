b7cdc10dd1411e62d52c330be457fb7536e82d9c
Brad Harms <brad.k.harms@gmail.com>
2018-06-29 16:29:53 -0500

    cleanup

--- src/Middleware/ConditionalRedirectMiddleware.php
+++ src/Middleware/ConditionalRedirectMiddleware.php
@@ -8,7 +8,16 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Diactoros\Response\RedirectResponse;
 
 /**
- * Middleware that conditionally redirects the user to the login page if they are not allowed to access the current page.
+ * Middleware that conditionally redirects the user to the login page.
+ * 
+ * Inject a callable object into this. When processing a request, it will be
+ * called, passing in the the request. If the callable returns false, the user
+ * will be redirected to the login page with a message indicating the user is
+ * not allowed to view the page and will be redirected back to the first page
+ * after logging in.
+ * 
+ * Callables can be function names, PHP "callable arrays", objects with the
+ * __invoke() method, or anything else PHP considers to be callable.
  */
 class ConditionalRedirectMiddleware implements MiddlewareInterface
 {
@@ -19,7 +28,7 @@ class ConditionalRedirectMiddleware implements MiddlewareInterface
      * @param callable $isAllowed Returns whether the user is allowed to view a response correlating the passed request
      */
     public function __construct(
-        callable $isAllowed 
+        callable $isAllowed
     ) {
         $this->isAllowed = $isAllowed;
     }
